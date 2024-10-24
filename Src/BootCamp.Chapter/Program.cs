087707be5cee651ef24ccd18531e5ea29d632ae7
ElDePacao <sam.drodriguez@gmail.com>
2024-10-23 22:58:35 -0700

    Refactor number 3
    Well refactored

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,96 +6,34 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            /* Homework 1/2 
+            string firstName = TakeUserInput("first name");
+            string lastName = TakeUserInput("last name");
+            string age = TakeUserInput("age");
+            string weight = TakeUserInput("weight in kg");
+            string height = TakeUserInput("height in cm");
 
-            Console.Write("Enter your first name: "); // ask user for first name
-            string firstName = Console.ReadLine(); // store inputed first name as string variable 
-            Console.Write("Enter your last name: "); // repeat above with last name
-            string lastName = Console.ReadLine(); 
-            Console.Write("Enter your age: "); // repeat with age
-            string age = Console.ReadLine();
-            Console.Write("Enter your weight in kg: "); // repeat with weight
-            string weight = Console.ReadLine();
-            Console.Write("Enter your height in cm: "); // repeat with height
-            string height = Console.ReadLine();
+            Console.WriteLine(InfoToString(firstName, lastName, age, weight, height));
 
-            // write out given data in string using either concatenation or interpolated string
-            Console.WriteLine(firstName + " " + lastName + " is " + age + " years old, they have a weight of " + weight + "kg and a height of " + height + "cm." ); // concatenation
-            Console.WriteLine($"{firstName} {lastName} is {age} years old, they have a weight of {weight}kg and a height of {height}cm."); // interpolation
-
-            int intAge = int.Parse(age);                // convert revelant inputs to int and double for further calculation
-            double doubleWeight = double.Parse(weight); 
-            double doubleHeight = double.Parse(height);
-
-
-            double heightInMeters = doubleHeight / 100; // convert height in cm to m
-            double bmi = Math.Round((doubleWeight / Math.Pow(heightInMeters, 2)), 3); // use formula to calculate BMI kg/(m*m) m being height in meters rounds it to 3 decimal places
-
-            Console.WriteLine($"{firstName} {lastName} has a BMI of {bmi}");
-            */         
-
-            // Homework 3: Refactor old code using functions
+            Console.WriteLine(BMI(weight, height));
         }
-            private static string TakeFirstName()
+            private static string TakeUserInput(string info)
             {
-                Console.Write("Enter your first name: "); // ask user for first name
-                string firstName = Console.ReadLine(); // store inputed first name as string variable 
-                return firstName;
-
+                Console.Write($"Enter your {info}: "); // ask user for a type of info
+                string UserInput = Console.ReadLine();
+                return UserInput;
             }
 
-            private static string TakeLastName()
+            private static string InfoToString(string firstName, string lastName, string age, string height, string weight)
             {
-                string lastName = Console.ReadLine();                 
-                Console.Write("Enter your age: ");
-                return lastName;
+                return $"User's name is {firstName} {lastName}. They are {age} years old. User has a height of {height}cm and a weight of {weight}kg";
             }
-
-            private static string TakeAge()
-            {
-                Console.Write("Enter your age: "); // repeat with age
-                string age = Console.ReadLine();
-                return age;
-            }
-
-            private static string TakeWeight()
-            {
-                Console.Write("Enter your weight in kg: "); // repeat with weight
-                string weight = Console.ReadLine();
-                return weight;
-            }
-
-            private static string TakeHeight()
-            {
-                Console.Write("Enter your height in cm: "); // repeat with height                
-                string height = Console.ReadLine();
-                return height;   
-            }
-                
-            
-
-            double BMI(string weight, string height, string age)
-            {
-                int intAge = int.Parse(age);                // convert revelant inputs to int and double for further calculation
+            private static string BMI(string weight, string height)
+            {              // convert revelant inputs to int and double for further calculation
                 double doubleWeight = double.Parse(weight); 
                 double doubleHeight = double.Parse(height);
                 double heightInMeters = doubleHeight / 100; // convert height in cm to m
-                double bmi = Math.Round((doubleWeight / Math.Pow(heightInMeters, 2)), 3);    
-                return bmi;    
-            }
-
-            private static string BMIString()
-            {
-                return $"{firstName} {lastName} is {age} years old, they have a weight of {weight}kg and a height of {height}cm. Making there BMI: {bmi}";
+                double bmi = Math.Round((doubleWeight / Math.Pow(heightInMeters, 2)), 3);
+                return $"User has a bmi of {bmi}.";
             } 
-
-            
-            
-
-
-
-
-
-         
     }
 }
